8dc1aa4dc31b95c6f4addeb6a50b862d4d306d43
fares-ma <farsagwa6@gmail.com>
2026-03-06 16:42:43 +0200

    Update build metadata and NuGet cache hashes
    Updated AssemblyInformationalVersion to new commit hash.
    Refreshed cache files to reflect latest build and dependency changes.

--- Core/Services/obj/Debug/net10.0/Services.AssemblyInfo.cs
+++ Core/Services/obj/Debug/net10.0/Services.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Services")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d7d552c7ebc6b815b35974e59108e4bd7fb858a8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b25b935ad06e0a770b7ac5db0e3b0de516da0fee")]
 [assembly: System.Reflection.AssemblyProductAttribute("Services")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Services")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
